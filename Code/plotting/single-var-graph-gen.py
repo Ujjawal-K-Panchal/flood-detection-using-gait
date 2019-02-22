92b23166584b01e600b28eb26663c5db5078d2e3
Ujjawal.K.Panchal <ujjawalpanchal32@gmail.com>
2019-02-22 15:40:19 +0000

    Update single-var-graph-gen.py

--- Code/plotting/single-var-graph-gen.py
+++ Code/plotting/single-var-graph-gen.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Title : generating plot of windowed data single variables vs index (time).
+Author : Ujjawal.K.Panchal
 
 """
 
